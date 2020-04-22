24f9e7e34d89bc46f27d0d86d294c4b43e91fdc5
DuongYenAnh <johndoe@example.com>
2020-04-22 17:44:25 +0700

    tai file

---
+++ lesson6.js
@@ -0,0 +1,45 @@
+// lớn hơn >
+// nhỏ hơn <
+// nhỏ hơn hoặc băng <=
+// lớn hơn hoặc bằng >=
+// chia lấy dư
+// khác !=,!==
+// bằng ==,=== (== so sanh gia tri cua bien,=== so sanh ca gia tri va kieu gia tri)
+// phủ định!
+// cộng +
+// trừ -
+// nhân *
+// chia /
+console.log(5=="5");
+
+// vòng lăp for: 
+var array=[1,2,3,4,5];
+for(var i=0;i<4;i<i++){
+    console.log(i);
+    break;
+}
+
+// vong lap khac:
+// array.forEach(function(item){
+//     console.log(item);
+// })
+
+var check =true;
+if(check==true){
+    console.log("day la phep tinh dung")
+}
+else{
+    console.log("day la phep tinh sai")
+}
+
+var crush={
+    hoTen: "Benedict Cumberbatch",
+    tuoiTac: "35",
+    chieuCao: "2m00",
+    canNang:"80kgs",}
+
+console.log(crush.hoTen);
+
+var arr= [1,5,7,8,9,15,8];
+for (var i=1;i<7;i=i*2)
+{console.log(i)}
